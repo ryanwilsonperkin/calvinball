ba26f2842d52075f2b109954659dd9e85a77b399
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-04-04 21:04:07 -0400

    Just save method.

--- calvinball/game.py
+++ calvinball/game.py
@@ -25,10 +25,11 @@ class Game(object):
         """Return true if action is allowed by current rules."""
         return all(rule.allows(action) for rule in self.rules)
 
-    def to_json(self):
-        """Serialize this Game instance to JSON."""
-        return json.dumps(
+    def save(self, json_fp):
+        """Save json serialization of this game to json_fp."""
+        json.dump(
             self,
+            json_fp,
             sort_keys=True,
             default=lambda o: o.__dict__,
             indent=4
